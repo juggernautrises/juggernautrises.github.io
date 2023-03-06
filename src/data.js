26cdf9eb88f8bda65ec112edbc5179209e5a162f
juggernautrises <ashoknayar@gmail.com>
2023-03-06 14:27:14 -0800

    lowered image filesizes

--- src/data.js
+++ src/data.js
@@ -4,35 +4,35 @@ export const projects = [
     title: "Iron Jug Mark XVII",
     description:
       "The second iteration of a robotic dog mask. This version included wireless control via an Android app that I wrote.",
-    image: "./images/ironjug_xvii.png",
+    image: "./images/ironjug_xvii.jpg",
     link: "https://vimeo.com/manage/videos/473690051",
   },
   {
     title: "Iron Jug Mark II",
     description:
       "The first Iron Jug mask, created using an Arduino along with some servos, LEDs, and lots of puppy patience.",
-    image: "./images/ironjug_ii.png",
+    image: "./images/ironjug_ii.jpg",
     link: "https://www.youtube.com/watch?v=PFtaLmJuq_o",
   },
   {
     title: "Tenet (on a Nokia LCD display)",
     description:
       "I decided to put the movie Tenet on a Nokia LCD screen for some reason! 🤷‍♀️",
-    image: "./images/tenet.png",
+    image: "./images/tenet.jpg",
     link: "https://www.youtube.com/watch?v=EbS7NhFajNg",
   },
   {
     title: "Beer Bot",
     description:
       "A little robot that poured my beer for me. I clearly underestimated the Coefficient of Foam!",
-    image: "./images/beerbot.png",
+    image: "./images/beerbot.jpg",
     link: "https://www.youtube.com/watch?v=Ngt4ThmIYvQ",
   },
   {
     title: "Sunset Park Riders - Pistol Grip",
     description:
       "A music video made for a friend's rap group. Did all the modeling, lighting, and animation myself over the course of a year.",
-    image: "./images/spr.png",
+    image: "./images/spr.jpg",
     link: "https://www.youtube.com/watch?v=Q6QxXqBpIGA",
   },
 
