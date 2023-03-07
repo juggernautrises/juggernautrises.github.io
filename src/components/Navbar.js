653c3fea3d7d598db508c72906efd867c0bf0150
juggernautrises <ashoknayar@gmail.com>
2023-03-06 17:06:23 -0800

    updated target links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,14 @@ export default function Navbar() {
                     <div className="ml-3 text-xl">ash nayar</div>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-                    <a href="http://github.com/juggernautrises" className="mr-5 hover:text-white">
-                        <img src={github} target="_blank" width="45px" alt="github" />
+                    <a href="http://github.com/juggernautrises" target="_blank" className="mr-5 hover:text-white">
+                        <img src={github} width="45px" alt="github" />
                     </a>
-                    <a href="https://www.linkedin.com/in/ash-nayar/" className="mr-5 hover:text-white">
-                        <img src={linkedin} target="_blank" width="50px" alt="linkedin" />
+                    <a href="https://www.linkedin.com/in/ash-nayar/" target="_blank" className="mr-5 hover:text-white">
+                        <img src={linkedin} width="50px" alt="linkedin" />
                     </a>
-                    <a href="https://www.imdb.com/name/nm2363452/" className="mr-5 hover:text-white">
-                        <img src={imdb} target="_blank" width="45px" alt="imdb" />
+                    <a href="https://www.imdb.com/name/nm2363452/" target="_blank" className="mr-5 hover:text-white">
+                        <img src={imdb} width="45px" alt="imdb" />
                     </a>
                 </nav>
                 <div></div>
